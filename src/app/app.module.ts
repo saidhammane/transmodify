c42632583ed787624407772cccb4529250441319
Said Hammane <said.hammane1@gmail.com>
2024-02-05 11:33:54 +0100

    UPDATES

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FileUploadComponent } from './file-upload/file-upload.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [
+    FileUploadComponent,
     AppComponent,
     FileUploadComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrl: './file-upload.component.css'
 })
 export class FileUploadComponent {
-  
+  selectedFile: File | null = null;
+
+  constructor(private http: HttpClient) {}
+
+  onFileSelected(event: any): void {
+    this.selectedFile = event.target.files[0];
+    console.log(this.selectedFile);
+  }
+
+
+  // uploadFile(): void {
+  //   if (this.selectedFile) {
+  //     const formData = new FormData();
+  //     let result = formData.append('file', this.selectedFile);
+  //     console.log(this.selectedFile);
+  //     this.http.post('/upload', formData)
+  //       .subscribe(response => {
+  //         console.log('File uploaded successfully:', response);
+  //       }, error => {
+  //         console.error('Error uploading file:', error);
+  //     });
+  //   }
+  // }
 }
